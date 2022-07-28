37a93cd847019d7e5d0d310a349d26c6588164cb
MSG <milk6645@gmail.com>
2022-07-28 17:08:29 +0900

    implement #3-enhance:

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         BubbleSort<String> sort = new BubbleSort<>();
         System.out.println("git 풀리퀘스트");
-        System.out.println("git 풀 리퀘스트 테스트");
+        System.out.println("git 풀 리퀘스트  테스트");
         System.out.println("{result} " + sort.sort(Arrays.asList()));
 
         // IOC를 적용하면?
