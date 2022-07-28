91b71817308c40f152f62a7649d448068f078ef0
MSG <milk6645@gmail.com>
2022-07-28 16:25:42 +0900

    Implement Main
    실행될 메인 로직 구현

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,11 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    @DisplayName("메인 테스트")
     @Test
     void main(){
-        Main.main(null);
+        String[] args = {"3", "1", "2"};
+
+        // When & Then
+        Main.main(args);
     }
 
 }
