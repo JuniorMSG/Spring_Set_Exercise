e7f3a880d9809b33da2d1be1afdf97c7e31ad1ea
MSG <milk6645@gmail.com>
2022-07-28 16:36:37 +0900

    git pull request
    한번 써보고 싶어서..

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         BubbleSort<String> sort = new BubbleSort<>();
-
+        System.out.println("git 풀리퀘스트");
         System.out.println("{result} " + sort.sort(Arrays.asList()));
     }
 }
